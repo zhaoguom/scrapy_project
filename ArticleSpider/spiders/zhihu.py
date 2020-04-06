87c6051f5e28975b1a5d5594590956406ba4e013
zhaoguom <zhaoguo.ma@gmail.com>
2020-04-06 10:47:44 +0000

    fix duplicate primary key error

---
+++ ArticleSpider/spiders/zhihu.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class ZhihuSpider(scrapy.Spider):
+    name = 'zhihu'
+    allowed_domains = ['www.zhihu.com']
+    start_urls = ['https://www.zhihu.com/']
+
+    def parse(self, response):
+        pass
